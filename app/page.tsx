8cf740d9465be92ad4e55c9bbf5d1fa88564a4b3
Lopezz647 <2qh4rhwbfg@privaterelay.appleid.com>
2026-05-20 12:00:10 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,6 @@ export default function Home() {
         <WhyJoin />
         <HowItWorks />  
         <ScoringSystem />       
-        <PricingCards />
         <FAQ />
       </main>
       <Footer />
