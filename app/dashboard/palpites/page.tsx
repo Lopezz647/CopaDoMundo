0c11178c0cbeb1ac70105fa44d9da7c84d9fd32d
Lopezz647 <2qh4rhwbfg@privaterelay.appleid.com>
2026-05-26 10:26:57 -0300

    Update page.tsx

--- app/dashboard/palpites/page.tsx
+++ app/dashboard/palpites/page.tsx
@@ -172,8 +172,8 @@ export default function Palpites() {
       <div className="pb-2">
         <UserHeader />
       </div>
-      <div className="mt-6 grid grid-cols-1 xl:grid-cols-[1fr_320px] gap-6">
-        <div className="flex flex-col gap-6">
+     <div className="mt-6 grid grid-cols-1 xl:grid-cols-[1fr_320px] gap-6">
+        <div className="flex flex-col gap-6"> {/* <--- O problema está aqui */}
           <PromoBanner />
 
           {/* O navegador recebe os novos controladores inteligentes de sincronização */}
