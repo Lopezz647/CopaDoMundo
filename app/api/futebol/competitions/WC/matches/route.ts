b7d3780d7335990d8e9394a5e92a11f1947ad734
Lopezz647 <2qh4rhwbfg@privaterelay.appleid.com>
2026-06-03 16:28:25 -0300

    Atualizar o route.ts

--- app/api/futebol/competitions/WC/matches/route.ts
+++ app/api/futebol/competitions/WC/matches/route.ts
@@ -1,10 +1,9 @@
+// app/api/futebol/competitions/WC/matches/route.ts
 import { NextResponse } from 'next/server'
 
-
-
 export async function GET() {
-    const API_KEY = process.env.NEXT_PUBLIC_API_TOKEN
-  
+  const API_KEY = process.env.NEXT_PUBLIC_API_TOKEN
+
   if (!API_KEY) {
     return NextResponse.json(
       { error: 'Token não configurado' },
@@ -19,44 +18,18 @@ export async function GET() {
         headers: {
           'X-Auth-Token': API_KEY,
         },
-        next: { revalidate: 30 },
+        next: { revalidate: 60 }, // ISR: revalidate a cada 60 segundos
       }
     )
 
     if (!res.ok) {
       const errorText = await res.text()
-
-      console.error(
-        `❌ ERRO FOOTBALL-DATA (Status ${res.status}):`,
-        errorText
-      )
-
+      console.error(`❌ ERRO FOOTBALL-DATA (Status ${res.status}):`, errorText)
       throw new Error(`Falha na API: Status ${res.status}`)
     }
+
     const data = await res.json()
-    // --- INÍCIO DO MOCK (MÁQUINA DO TEMPO) ---
-    const minutosAdicionais = -10; 
-
-    const jogoFalso = {
-      id: 999991,
-      competition: { id: "WC", name: "FIFA World Cup" },
-      homeTeam: { name: "Brasil", shortName: "BRA", crest: "https://crests.football-data.org/764.svg" },
-      awayTeam: { name: "Argentina", shortName: "ARG", crest: "https://crests.football-data.org/762.png" },
-      utcDate: new Date(Date.now() + minutosAdicionais * 60000).toISOString(), 
-      status: "IN_PLAY",
-      score: {
-        fullTime: {home: 3, away: 1}
-      }
-    };
-
-    // Garante que o array existe e injeta o jogo falso no topo
-    if (data.matches) {
-      data.matches.unshift(jogoFalso);
-    }
-    // --- FIM DO MOCK ---
-    
-    // Log para confirmar que os dados chegaram no servidor
-    console.log(`✅ Sucesso! Foram encontrados ${data.matches?.length || 0} jogos na API.`);
+    console.log(`✅ Sucesso! Foram encontrados ${data.matches?.length || 0} jogos.`)
     
     return NextResponse.json(data)
   } catch (error: any) {
