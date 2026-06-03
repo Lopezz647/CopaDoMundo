63ec655e55fea4ddb01e7306239ef844e0dce7cf
Lopezz647 <2qh4rhwbfg@privaterelay.appleid.com>
2026-06-03 17:37:26 -0300

    Atualizar o base44Client.ts

--- api/base44Client.ts
+++ api/base44Client.ts
@@ -1,14 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-import { appParams } from '@/lib/app-params';
 
-const { appId, token, functionsVersion, appBaseUrl } = appParams;
-
-//Create a client with authentication required
-export const base44 = createClient({
-  appId,
-  token,
-  functionsVersion,
-  serverUrl: '',
-  requiresAuth: false,
-  appBaseUrl
-});
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
