dbe9a1b3fb20b6ddee5114cba4cb470c76c81004
Lopezz647 <2qh4rhwbfg@privaterelay.appleid.com>
2026-06-11 17:42:27 -0300

    Atualizar o PredictionsModal.tsx

--- components/bolao/PredictionsModal.tsx
+++ components/bolao/PredictionsModal.tsx
@@ -1,4 +1,4 @@
-Import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { X, User, Clock, Loader2 } from "lucide-react";
 import { createClient } from "@/lib/supabase/client";
 
